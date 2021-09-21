e93479d42edd74d62d98d773164b2aa0e1cca16b
Joaquin Perez <joaquin@nurelm.com>
2021-09-21 15:11:01 -0300

    Random order of tests and mockserver
    Added random order for tests.
    Added MockServer and configured mocks for uuid-api and ingest-api.

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker-compose -f docker-compose.yml -f docker-compose.development.yml run --rm web sh -c "pip install -e . && pytest -vv"
+docker-compose -f docker-compose.yml -f docker-compose.development.yml run --rm web sh -c "pip install -e . && pytest -vv --random-order"
