5a2b52e4f0e3604afcb1100857b8c07090c3a168
Joaquin Perez <joaquin@nurelm.com>
2021-11-12 13:15:05 -0300

    Added Antibody table for search results.

---
+++ server/antibodyapi/hubmap/static/js/components/AntibodyHitsTable.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class AntibodyHitsTable extends React.Component {
+
+  render(){
+    const { hits } = this.props
+    console.log(hits);
+    var antibodies = '';
+    for (var i = 0; i < hits.length; i++) {
+      var hit = hits[i];
+      antibodies += `<tr key=${hit._id}>`;
+      antibodies += `<td>${hit._source.antibody_name}</td>`;
+      antibodies += `<td>${hit._source.host_organism}</td>`;
+      antibodies += `<td>${hit._source.target_name}</td>`;
+      antibodies += `<td>${hit._source.vendor}</td>`;
+      antibodies += `</tr>`;
+    }
+    return (
+      <div style={{width: '100%', boxSizing: 'border-box', padding: 8}}>
+        <table className="sk-table sk-table-striped" style={{width: '100%', boxSizing: 'border-box'}}>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Host Organism</th>
+              <th>Target Name</th>
+              <th>Vendor</th>
+            </tr>
+          </thead>
+          <tbody dangerouslySetInnerHTML={{__html: antibodies}}/>
+        </table>
+      </div>
+    )
+  }
+}
+
+export default AntibodyHitsTable;
--- server/antibodyapi/hubmap/static/js/components/Search.js
+++ server/antibodyapi/hubmap/static/js/components/Search.js
@@ -4,12 +4,13 @@ import {
   SearchkitManager, SearchkitProvider, SearchBox, Hits,
   Layout, TopBar, LayoutBody, SideBar, HierarchicalMenuFilter,
   RefinementListFilter, ActionBar, LayoutResults, HitsStats,
-  ActionBarRow, SelectedFilters, ResetFilters, MovieHitsGridItem,
-  NoHits
+  ActionBarRow, SelectedFilters, ResetFilters, NoHits
 } from "searchkit";
 
+import AntibodyHitsTable from './AntibodyHitsTable';
+
 const searchkit = new SearchkitManager("/");
- 
+
 const Search = () => (
   <SearchkitProvider searchkit={searchkit}>
   <Layout>
@@ -17,20 +18,24 @@ const Search = () => (
       <SearchBox
         autofocus={true}
         searchOnChange={true}
-        prefixQueryFields={["actors^1","type^2","languages","title^10"]}/>
+        prefixQueryFields={["antibody_name.keyword^3","target_name.keyword^2","uniprot_accession_number", "catalog_number", "organ_or_tissue.keyword"]}/>
     </TopBar>
 
     <LayoutBody>
       <SideBar>
         <HierarchicalMenuFilter
-          fields={["type.raw", "genres.raw"]}
-          title="Categories"
-          id="categories"/>
+          fields={["target_name.keyword"]}
+          title="Target Name"
+          id="target_names"/>
+        <HierarchicalMenuFilter
+          fields={["vendor.keyword"]}
+          title="Vendors"
+          id="vendors"/>
         <RefinementListFilter
-          id="actors"
-          title="Actors"
-          field="actors.raw"
-          operator="AND"
+          id="host_organism"
+          title="Host Organism"
+          field="host_organism.keyword"
+          operator="OR"
           size={10}/>
       </SideBar>
       <LayoutResults>
@@ -46,13 +51,13 @@ const Search = () => (
           </ActionBarRow>
 
         </ActionBar>
-        <Hits mod="sk-hits-grid" hitsPerPage={10} itemComponent={MovieHitsGridItem}
-          sourceFilter={["title", "poster", "imdbId"]}/>
+        <Hits mod="sk-hits-list" hitsPerPage={50} listComponent={AntibodyHitsTable}
+          sourceFilter={["antibody_name", "target_name", "host_organism", "vendor"]}/>
         <NoHits/>
       </LayoutResults>
     </LayoutBody>
   </Layout>
 </SearchkitProvider>
 );
- 
+
 export default Search;
