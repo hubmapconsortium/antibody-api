5668f8405c7ede9be28109678ba218d763034d5d
ChuckKollar <chuck@kollar.com>
2023-02-22 15:53:58 -0500

    Fixed bug in CSV download

--- server/antibodyapi/hubmap/static/js/components/DownloadFile.js
+++ server/antibodyapi/hubmap/static/js/components/DownloadFile.js
@@ -23,12 +23,11 @@ class DownloadFile extends SearchkitComponent {
         query.from = 0;
         console.info('this.searchkit.results: ', JSON.parse(JSON.stringify(this.searchkit.results)));
 
-        // Only the columns that the user is viewing, and not all of the columns...
-        // var _source = [];
-        // csv_column_order.forEach((key) => {
-        //     if (display[key] == 'table-cell') _source.push(key);
-        // })
-        query._source = csv_column_order;
+        var _source = [];
+        csv_column_order.forEach((key) => {
+            _source.push(key);
+        })
+        query._source = _source;
         if (this.avr_file_as_url && _source.includes('avr_pdf_filename')) {
             query._source = _source.concat('avr_pdf_uuid');
         }
