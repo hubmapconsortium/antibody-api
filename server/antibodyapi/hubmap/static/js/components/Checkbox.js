50a2c0c667d88de993f491088887f2a6429b8977
ChuckKollar <chuck@kollar.com>
2023-07-17 16:43:32 -0400

    test some changes on Checkbox

--- server/antibodyapi/hubmap/static/js/components/Checkbox.js
+++ server/antibodyapi/hubmap/static/js/components/Checkbox.js
@@ -3,21 +3,27 @@ import { useCookies } from 'react-cookie';
 
 function Checkbox(props) {
 
-  const [cookies, setCookie] = useCookies([]);
   const elt = props.element;
+  const label = props.label;
+  const [cookies, setCookie] = useCookies([]);
   const [checked, setChecked] = useState(cookies[elt]==='true');
   const id_col = elt + '_col'
   const id_header = id_col + "_head";
-  const label = props.label;
-  //console.info('elt: ', elt, ' id_col: ', id_col, ' id_header: ', id_header, ' label: ', label);
-  //console.info('display: ', display);
-  //console.info('cookie_checked: on entry', cookies[elt]);
+  console.info('elt: ', elt, ' id_col: ', id_col, ' id_header: ', id_header, ' label: ', label);
+  console.info('display: ', display);
+  console.info('cookie_checked: on entry', cookies[elt]);
+
+  const [checkboxState, setCheckboxState] = useState()
+  if (checkboxState[elt] === undefined) {
+    setCheckboxState(Object.assign(checkboxState, {elt: false}))
+  }
+  console.info('checkboxState: on entry', checkboxState);
 
   const handleChange = () => {
-    //console.info(elt, "checked on entry:", checked);
+    console.info(elt, "checked on entry:", checked);
     setChecked(!checked);
     setCookie(elt, checked?'false':'true', { path: "/", sameSite: 'strict' });
-    //console.info('cookie_checked after setCookie: ', cookies[elt]);
+    console.info('cookie_checked after setCookie: ', cookies[elt]);
     if (checked) {
       display[elt]="none";
       var all_col=document.getElementsByClassName(id_col);
