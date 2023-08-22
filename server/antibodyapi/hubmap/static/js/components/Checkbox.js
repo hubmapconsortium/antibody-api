9df08c4dacf235e9718ab7ef00f12219acac0fe0
ChuckKollar <chuck@kollar.com>
2023-08-22 12:29:00 -0400

    Getting Minified React error #185 so limiting size of checkbox_props to see if that is the cause

--- server/antibodyapi/hubmap/static/js/components/Checkbox.js
+++ server/antibodyapi/hubmap/static/js/components/Checkbox.js
@@ -6,29 +6,6 @@ function AdditionalColumns() {
   const checkbox_props = [
     {element:"clone_id", label:"Clone ID"},
     {element:"cell_line", label:"Cell Line"},
-    {element:"cell_line_ontology_id", label:"Cell Line Ontology ID"},
-    {element:"host", label:"Host"},
-    {element:"rrid", label:"RRID"},
-    {element:"catalog_number", label:"Catalog#"},
-    {element:"lot_number", label:"Lot#"},
-    {element:"vendor_name", label:"Vendor"},
-    {element:"recombinant", label:"Recombinant"},
-    {element:"organ", label:"Organ"},
-    {element:"author_orcids", label:"Author ORCiDs"},
-    {element:"hgnc_id", label:"HGNC ID"},
-    {element:"isotype", label:"Isotype"},
-    {element:"concentration_value", label:"Concentration"},
-    {element:"dilution_factor", label:"Dilution Factor"},
-    {element:"conjugate", label:"Conjugate"},
-    {element:"cycle_number", label:"Cycle#"},
-    {element:"fluorescent_reporter", label:"Fluorescent Reporter"},
-    {element:"manuscript_doi", label:"Manuscript DOI"},
-    {element:"protocol_doi", label:"Protocol DOI"},
-    {element:"vendor_affiliation", label:"Vendor Affiliation"},
-    {element:"organ_uberon_id", label:"Organ UBERON ID"},
-    {element:"antigen_retrieval", label:"Antigen Retrieval"},
-    {element:"omap_id", label:"OMAP ID"},
-    {element:"created_by_user_email", label:"Submitter Email"}
   ];
 
   const state_values = Object.assign({}, ...checkbox_props.map((x) => ({[x.element]: false})));
@@ -39,7 +16,7 @@ function AdditionalColumns() {
   const handleChange = (elt) => {
     console.info(elt, "Checkbox handler: checked before:", checked, ' elt: ', elt);
     setChecked({...checked, ...{elt: !checked.elt}})
-    console.info('Checkbox handler: chected after: ', checked, ' elt: ', elt);
+    console.info('Checkbox handler: checked after: ', checked, ' elt: ', elt);
     display[elt] = checked.elt?'table-cell':'none';
     const id_col = elt + '_col';
     const all_col=document.getElementsByClassName(id_col);
