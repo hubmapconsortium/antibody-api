1c71c73d38c8e10f06c85dcc0eea8e70e9edc300
ChuckKollar <chuck@kollar.com>
2023-08-23 10:25:33 -0400

    Added other columns back and more debugging

--- server/antibodyapi/hubmap/static/js/components/AdditionalColumns.js
+++ server/antibodyapi/hubmap/static/js/components/AdditionalColumns.js
@@ -7,23 +7,46 @@ function AdditionalColumns() {
   const checkbox_props = [
     {element:"clone_id", label:"Clone ID"},
     {element:"cell_line", label:"Cell Line"},
+    {element:"cell_line_ontology_id", label:"Cell Line Ontology ID"},
+    {element:"host", label:"Host"},
+    {element:"rrid", label:"RRID"},
+    {element:"catalog_number", label:"Catalog#"},
+    {element:"lot_number", label:"Lot#"},
+    {element:"vendor_name", label:"Vendor"},
+    {element:"recombinant", label:"Recombinant"},
+    {element:"organ", label:"Organ"},
+    {element:"author_orcids", label:"Author ORCiDs"},
+    {element:"hgnc_id", label:"HGNC ID"},
+    {element:"isotype", label:"Isotype"},
+    {element:"concentration_value", label:"Concentration"},
+    {element:"dilution_factor", label:"Dilution Factor"},
+    {element:"conjugate", label:"Conjugate"},
+    {element:"cycle_number", label:"Cycle#"},
+    {element:"fluorescent_reporter", label:"Fluorescent Reporter"},
+    {element:"manuscript_doi", label:"Manuscript DOI"},
+    {element:"protocol_doi", label:"Protocol DOI"},
+    {element:"vendor_affiliation", label:"Vendor Affiliation"},
+    {element:"organ_uberon_id", label:"Organ UBERON ID"},
+    {element:"antigen_retrieval", label:"Antigen Retrieval"},
+    {element:"omap_id", label:"OMAP ID"},
+    {element:"created_by_user_email", label:"Submitter Email"},
   ];
 
   const state_values = Object.assign({}, ...checkbox_props.map((x) => ({[x.element]: false})));
   const [checked, setChecked] = useState(state_values);
-  console.info('Set useState for state_values; checked:', checked);
+  console.info('AdditionalColumns' after set useState for state_values; checked:', checked);
 
   const handleChange = (elt) => {
-    console.info(elt, "Checkbox handler: checked before:", checked, ' elt: ', elt);
+    console.info(elt, "Checkbox handleChange: checked before:", checked, ' elt: ', elt, ' !checked[elt]: ', !checked[elt]);
     setChecked({...checked, ...{elt: !checked[elt]}});
-    console.info('Checkbox handler: checked after: ', checked, ' elt: ', elt);
+    console.info('Checkbox handleChange: checked after: ', checked, ' elt: ', elt);
     display[elt] = checked[elt]?'table-cell':'none';
     const id_col = elt + '_col';
     const all_col=document.getElementsByClassName(id_col);
     for (var i=0;i<all_col.length;i++) {
        all_col[i].style.display=display[elt];
     }
-    console.info('Checkbox handler: display after: ', display);
+    console.info('Checkbox handleChange: display after: ', display);
     // Uncaught TypeError: document.getElementById(...) is null
     // will only happen if no data has been loaded
     const id_header = id_col + "_head";
